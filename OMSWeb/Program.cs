e12468b0ebbfcc22386c744dc17e89b5ff74f9af
Sergiusz Kriwobokow <writesd@hotmail.com>
2019-05-28 19:08:08 +0200

    added logging subsystem on base Serilog

--- OMSWeb/Program.cs
+++ OMSWeb/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 using System.IO;
 
-//TODO: Implementing Logging
 //TODO: Implementing Cashing
-//TODO: Implementing Authentifikation and Authorisation
+//TODO: Implementing Authentification and Authorisation
 //TODO: Implementing Migrations
 //TODO: Add CategoriesQueryProcessorTest
 //TODO: Add CustomersQueryProcessorTest
@@ -18,20 +20,37 @@ namespace OMSWeb
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File("Logs\\log.txt", rollingInterval: RollingInterval.Hour)
+            //.WriteTo.RollingFile("Logs\\Web-{Date}.log", outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
 
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseIISIntegration()
-              .UseContentRoot(Directory.GetCurrentDirectory())
-              .UseStartup<Startup>()
-              .Build();
-
-            host.Run();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost CreateWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
     }
 }
